1da212bc92bb1a63198aaeb8163ad6ca0d31018b
AdminJonatan <Jonatan_gomez23191@elpoli.edu.co>
2022-02-10 16:30:02 -0500

    drtcs

--- src/main/java/co/com/pds/demo/repositorio/ShowtimesRepository.java
+++
@@ -1,9 +0,0 @@
-package co.com.pds.demo.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.com.pds.demo.entidades.Showtimes;
-
-public interface ShowtimesRepository   extends JpaRepository<Showtimes, Long>{
-
-}
